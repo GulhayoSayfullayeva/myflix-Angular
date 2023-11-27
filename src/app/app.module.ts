40777c241bcdf88bbf4f78a4d52d20d9856ac67a
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-11-27 15:09:33 +0100

    registration component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
